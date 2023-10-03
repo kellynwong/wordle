4a46098fe62dea00f4853cfc4ff48227391c34c5
kellyn <kellynwong@hotmail.com>
2023-10-03 23:05:33 +0800

    Set up pseudo codes

---
+++ script.js
@@ -0,0 +1,11 @@
+// Choose a random 5 letter word (from an array of valid English words) when DOM is loaded
+// Generate a row of 6 squares (row) when DOM is loaded
+// Validate user's input - that they are alphabets
+// Generate button to submit word once user has entered 5 alphabets
+// Display winning message if all 5 spots match
+// Colour box green if letter guessed is in correct spot
+// Colour box orange if letter guessed is in wrong spot
+// Colour box grey if letter does not exist in word
+// Check if user input matches random word
+// Move on to next row for next guess
+// Display losing message if all 6 rows have been guessed without success
