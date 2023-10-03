d741bd71050a504ffaccb2eec1cdfdd41264fe0c
kellyn <kellynwong@hotmail.com>
2023-10-03 23:54:03 +0800

    Create function to select a random word for the game

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
 // Choose a random 5 letter word (from an array of valid English words) when DOM is loaded
+arrayOfEnglishWords = [
+  "dream",
+  "guard",
+  "flood",
+  "adult",
+  "sight",
+  "alarm",
+  "force",
+  "wound",
+  "brave",
+  "cable",
+  "panic",
+  "study",
+  "faith",
+  "equal",
+  "grade",
+  "award",
+  "bully",
+  "voice",
+  "drive",
+  "title",
+  "sully",
+];
+let userInput = [];
+let randomWordArray = [];
+let randomWordString;
+let numOfTries = 0;
+selectWordForGame();
+
+function selectWordForGame() {
+  randomWordString =
+    arrayOfEnglishWords[
+      Math.floor(Math.random() * arrayOfEnglishWords.length + 1)
+    ]; // Returns a random integer from 0 to 18 (since length is 19), so need to add 1
+
+  randomWordArray = randomWordString.split("");
+}
+
+console.log(randomWordArray);
+
 // Generate a row of 6 squares (row) when DOM is loaded
 // Validate user's input - that they are alphabets
 // Generate button to submit word once user has entered 5 alphabets
