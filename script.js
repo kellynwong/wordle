b2e61c7ce249193e197b54ad0e23f03a824a761c
kellyn <kellynwong@hotmail.com>
2023-10-05 02:09:02 +0800

    Create function to check user input

--- script.js
+++ script.js
@@ -117,10 +117,64 @@ function generateButton() {
   button.addEventListener("click", checkWord);
 }
 
+// Check if user input matches random word; useful guide: https://www.javascripttutorial.net/es6/javascript-array-findindex/
 // Display winning message if all 5 spots match
 // Colour box green if letter guessed is in correct spot
 // Colour box orange if letter guessed is in wrong spot
 // Colour box grey if letter does not exist in word
-// Check if user input matches random word
+
+function checkWord() {
+  console.log(userInput);
+  console.log(randomWordArray);
+  if (userInput.join() === randomWordArray.join()) {
+    let winMessage = document.createElement("div");
+    winMessage.setAttribute("id", "winning-message");
+    winMessage.innerHTML = "You have won";
+    document.body.append(winMessage);
+  }
+
+  let index = [];
+  for (let a = 0; a < randomWordArray.length; a++) {
+    if (randomWordArray[a] === userInput[a]) {
+      document
+        .getElementById(numOfTries.toString() + a.toString())
+        .setAttribute("class", "green");
+      randomWordArray[a] = "9";
+      userInput[a] = "9";
+      index.push(a);
+    }
+  }
+  console.log(index);
+  console.log(randomWordArray);
+  console.log(userInput);
+
+  for (let a = 0; a < userInput.length; a++) {
+    if (randomWordArray.includes(userInput[a]) && !index.includes(a)) {
+      document
+        .getElementById(numOfTries.toString() + a.toString())
+        .setAttribute("class", "orange");
+      let num = randomWordArray.findIndex((letter) => letter === userInput[a]);
+      randomWordArray[num] = "9";
+      userInput[a] = "9";
+      index.push(a);
+    } else if (randomWordArray[a] != userInput[a])
+      document
+        .getElementById(numOfTries.toString() + a.toString())
+        .setAttribute("class", "grey");
+  }
+  moveToNextRow();
+}
+
 // Move on to next row for next guess
+function moveToNextRow() {
+  console.log("hi");
+  let button = document.getElementById("submit-done");
+  button.remove();
+  userInput = [];
+  generateRow();
+  randomWordArray = randomWordString.split("");
+}
+
 // Display losing message if all 6 rows have been guessed without success
+
+// Solve backspace and keypress problem
